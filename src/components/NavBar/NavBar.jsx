f38763999185d468ee748b676a8905f21a3a8005
Rafael Ibarra <rafbar2000rr@gmail.com>
2023-06-30 17:38:12 -0500

    icono del carrito

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import style from "./NavBar.module.css"
+import Badge from '@mui/material/Badge';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export default function NavBar(size) {
   return (
@@ -10,6 +13,11 @@ export default function NavBar(size) {
       </div> */}
       <div>
         <span className={style.cart} > Carrito: {size.size} </span>
+        <IconButton aria-label="mostrar items" color= 'inherit'>
+              <Badge badgeContent={size.size} color="secondary">
+               <ShoppingCartIcon fontSize="large" color='white'/>
+              </Badge>
+        </IconButton>
       </div>
     </nav>
     
