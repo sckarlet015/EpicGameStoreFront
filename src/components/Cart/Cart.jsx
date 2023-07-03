8b033c6a0399e9fb6906856013b47e4da6a5ea7c
sckarlet015 <sckarlet015@gmail.com>
2023-07-03 13:32:17 -0600

    arreglo array

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,76 +1,70 @@
-import { useState, useEffect } from "react"
-import { useSelector} from "react-redux"
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
-import style from "./Cart.module.css"
+import style from "./Cart.module.css";
 import axios from "axios";
 import MercadoPago from "./MercadoPago/MercadoPago";
 
+const Cart = () => {
+  const dataUser = useSelector((state) => state.dataUser);
+  const [price, setPrice] = useState(0);
+  const [cart, setCart] = useState([]);
+  const history = useHistory();
+  const handleDataCart = async () => {
+    const cartID = dataUser.cartID;
+    const response = await axios.get(`http://localhost:3001/cart/${cartID}`);
+    setCart(response.data[0]?.Videogames);
+  };
 
-export default function Cart() {
+  useEffect(() => {
+    handleDataCart();
+    handlePrice();
+  }, []);
 
-    const dataUser = useSelector((state) => state.dataUser)
-    const [price, setPrice] = useState(0)
-    const [cart, setCart] = useState([])
+  const handlePrice = () => {
+    const total = cart.reduce((accumulator, item) => {
+      return accumulator + item.quantity * item.price;
+    }, 0);
+    setPrice(total);
+  };
 
-
-    // console.log(dataUser);
-
-    const handleDataCart = async () => {
-        
-        // const cartID =  {
-        //     cartID: dataUser.cartID
-        // };
-        const cartID = dataUser.cartID
-
-        const response = await (await axios.get(`http://localhost:3001/cart/${cartID}`)).data
-        // console.log(response[0].Videogames);
-        setCart(response[0]?.Videogames)
-    }
-
-    console.log(cart);
-
-    useEffect(() => {
-        handleDataCart();
-        handlePrice();
-    },[])
-
-    const handlePrice = ()=>{
-        let total = 0;
-        cart.map((item)=>(
-            total += item.quantity * item.price
+  return (
+    <div className={style.all}>
+      <div>
+        <NavBar />
+      </div>
+      {cart.length > 0 ? (
+        cart.map((item) => (
+          <div key={item.id} className={style.cart_box}>
+            <div className={style.cart_img}>
+              <img src={item.image} alt={item.title} className={style.image} />
+              <p>{item.title}</p>
+            </div>
+            <div>
+              <p>{item.quantity}</p>
+            </div>
+            <div>
+              <span>{item.unit_price}</span>
+              <button>Remove</button>
+            </div>
+          </div>
         ))
-        setPrice(total);
-    }
-
-    return (
-        <div className={style.all}>
-            <div>
-                <NavBar />
-            </div>
-            {
-                cart.map((item) => (
-                    <div key={item.id} className={style.cart_box}>
-                    <div className={style.cart_img}>
-                        <img src={item.image} alt={item.title} className={style.image} />
-                        <p>{item.title}</p>
-                    </div>
-                    <div>
-                        <p>{item.quantity}</p>
-                    </div>
-                    <div>
-                        <span>{item.unit_price}</span>
-                        <button>Remove</button>
-                    </div>
-                </div>
-                ))
-            }
-            <div className={style.total}>
-                <span>Total Price of your Cart</span>
-                <span>{price}</span>
-            </div>
-            <div>
-                <MercadoPago/>
-            </div>
+      ) : (
+        <div className={style.emptyCart}>
+          <p>Aún no hay juegos en tu carrito</p>
+          <button onClick={() => history.push("/")}>Ir al Home</button>
         </div>
-    )
-}
+      )}
+      <div className={style.total}>
+        <span>Total Price of your Cart</span>
+        <span>{price}</span>
+      </div>
+      <div>
+        <MercadoPago />
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
